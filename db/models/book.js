11296eb015e87999e63597f86da1348db1fac7cb
bplsmolders <79159158+bplsmolders@users.noreply.github.com>
2021-04-09 16:09:58 +0200

    adds basics

---
+++ db/models/book.js
@@ -0,0 +1,29 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  const Book = sequelize.define('Book', {
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notEmpty: {
+          msg: '"Author" is required'
+        }
+      }
+    },
+    author: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notEmpty: {
+          msg: '"Author" is required'
+        }
+      }
+    },
+    genre: DataTypes.STRING,
+    year: DataTypes.INTEGER
+  }, {});
+  Book.associate = function(models) {
+    // associations can be defined here
+  };
+  return Book;
+};
